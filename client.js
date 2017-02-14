7cc000cc480fc3166b515e1a379c251efe6cc5da
Chad Sundberg <chadsundberg@gmail.com>
2017-02-14 09:54:32 -0600

    Updating Object Creation

--- client.js
+++ client.js
@@ -1,19 +1,39 @@
 $(document).ready(function(){ // waits for DOM to load
-  $('#submitNewEmployee').on('click', function(){ // event listener on submitNewEmployee
-    // declaring variables and retrieving values from input boxes
-    var firstName = $('#firstName').val();
-    var lastName = $('#lastName').val();
-    var idNumber = $('#idNumber').val();
-    var jobTitle = $('#jobTitle').val();
-    var annualSalary = $('#annualSalary').val();
+     $('form').on('submit', function(event){ // listens for button clicks inside the form
+  // $('#submitNewEmployee').on('click', function(){ // event listener on submitNewEmployee
+      event.preventDefault();
+
+      console.log('form values: ', $(this).serializeArray()); //  is looking at the form and creating Objects out of the array
+// create an array of the inputs, the inputs are converted to Objects.
+// Objects have two properties: name and value.
+// { name: 'firstName', value: 'Luke'}
+  var submissionArray = $(this).serializeArray();
+  var newEmployeeObject = {};  // {firstName: 'Luke', lastName: 'Schlangen'}
+
+  submissionArray.forEach(function(inputFieldObject){
+    newEmployeeObject[inputFieldObject.name] = inputFieldObject.value;  // the first time through the array, newEmployeeObject is an empty Object {}
+    // same as saying newEmployeeObject.firstName  = Luke;
+    // this code allows you to have as many values as you want and assign them to an Object.
+    // the second time through the loop inputField is lastName, and the value is Schlangen and so on.
+  // });
+
+  // the above two lines of code does the same as the three lines below.
+
+  // for (var i = 0; i < submissionArray.length; i++) {
+  //   var inputFieldObject = submissionArray[i];
+  //   newEmployeeObject[inputFieldObject.name] = inputFieldObject.value;
+  // }
+console.log('New Employee Object', newEmployeeObject);
+
+
 // this section is adding new employee row to the DOM
 $('#employeeTableBody').append(
     '<tr>' +
-      '<td>' + firstName + '</td>' +
-      '<td>' + lastName + '</td>' +
-      '<td>' + idNumber + '</td>' +
-      '<td>' + jobTitle + '</td>' +
-      '<td>' + annualSalary + '</td>' +
+      '<td>' + newEmployeeObject.firstName + '</td>' +
+      '<td>' + newEmployeeObject.lastName + '</td>' +
+      '<td>' + newEmployeeObject.idNumber + '</td>' +
+      '<td>' + newEmployeeObject.jobTitle + '</td>' +
+      '<td>' + newEmployeeObject.annualSalary + '</td>' +
       '<td><button class="deleteEmployeeButton" data-salary="' + annualSalary + '">Delete</button></td>' +
     '</tr>'
   );  // data(), can be stored on the DOM.
